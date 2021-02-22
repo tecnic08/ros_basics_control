aa93417fbe2999c4c461b53500d4a4806971edfe
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-02-22 10:34:48 +0100

    Add sensor publishers for simulated Thymio to match custom msg

--- include/ros_basics_control/thymio_simu_interface.h
+++ include/ros_basics_control/thymio_simu_interface.h
@@ -3,6 +3,10 @@
 
 #include <ros_basics_control/thymio_interface_base.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <ros_basics_msgs/ProximitySensors.h>
+
+#include <mutex>
 
 namespace ros_tp {
 
@@ -13,7 +17,22 @@ namespace ros_tp {
         virtual void set_target_velocities(double v, double z);
 
     protected:
+        void _prox_s0_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s1_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s2_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s3_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s4_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s5_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _prox_s6_cb(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void _publish_prox_values(double value, int idx);
+        std::atomic<size_t> _sensor_counter;
+
         ros::Publisher _vel_pub;
+        ros::Publisher _prox_sensor_pub;
+        std::vector<ros::Subscriber> _laser_subs;
+        std::vector<double> _prox_sensor_values;
+
+        std::mutex _pval_lock;
     };
 
 } // namespace ros_tp
